903cba76d4adefd758a53058bb1a16b27610b5c7
Jacob Gomez <jakegomez814@hotmail.com>
2022-07-10 20:40:58 -0400

    dictionaries, save/load data with JSON

--- Scripts/GameOverScreen.gd
+++ Scripts/GameOverScreen.gd
@@ -1,5 +1,14 @@
 extends Control
 
+onready var highscoreLabel = $HighScoreLabel
+
+func _ready():
+	set_highscore_label()
+	
+func set_highscore_label():
+	var save_data = SaveAndLoad.load_data_from_file()
+	highscoreLabel.text = "Highscore : " + str(save_data.highscore)
+
 func _process(delta):
 	if Input.is_action_just_pressed("ui_cancel"):
 		get_tree().change_scene("res://Scenes/StartMenu.tscn")
